771b9d73d9dfe920a5397ced1d393a3fa23371a0
Knight <knzphumthawan@gmail.com>
2021-08-30 14:26:31 +0700

    add main

--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"golang-healthcheck/healthcheck"
+	"golang-healthcheck/readcsv"
 	"net/http"
 )
 
 func main() {
 	router := mux.NewRouter()
+
+	r := readcsv.NewReadCSV("test.csv")
+	links := r.ReaderCSV()
+	healthcheck.NewHealthCheck(links)
 
 	http.ListenAndServe(":8080", router)
 }
