bb014c36ba63cebc1b8402ea395cc95e8dc5385e
Knight <knzphumthawan@gmail.com>
2021-08-30 16:22:32 +0700

    add handler

---
+++ handler/handler.go
@@ -0,0 +1,39 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type handler struct {
+	ClientID    string
+	RedirectUri string
+}
+
+type Handler interface {
+	RedirectLogin(writer http.ResponseWriter, request *http.Request)
+	CallBack(writer http.ResponseWriter, request *http.Request)
+}
+
+func NewHandler(clientID string, redirectUri string) Handler {
+	return &handler{
+		ClientID:    clientID,
+		RedirectUri: redirectUri,
+	}
+}
+
+func (h *handler) RedirectLogin(writer http.ResponseWriter, request *http.Request) {
+	responseType := "code"
+	state := "12345abcde"
+	scope := "profile%20openid"
+	nonce := "09876xyz"
+	url := fmt.Sprintf("https://access.line.me/oauth2/v2.1/authorize?response_type=%s&client_id=%s&redirect_uri=%s&state=%s&scope=%s&nonce=%s", responseType, h.ClientID, h.RedirectUri, state, scope, nonce)
+	http.Redirect(writer, request, url, http.StatusFound)
+}
+
+func (h *handler) CallBack(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println("GET params were:", request.URL.Query())
+	code := request.URL.Query().Get("code")
+	writer.WriteHeader(http.StatusOK)
+	fmt.Fprintln(writer, code)
+}
