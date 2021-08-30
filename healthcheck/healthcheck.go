fe7d471a64ee1ff1f2c085f4ea49d53b9b3847f4
Knight <knzphumthawan@gmail.com>
2021-08-30 23:21:36 +0700

    mock health check

--- healthcheck/healthcheck.go
+++ healthcheck/healthcheck.go
@@ -7,34 +7,35 @@ import (
 )
 
 type healthCheck struct {
-	Links []string
+	Links     []string
+	WaitGroup *sync.WaitGroup
 }
 
 type HealthCheck interface {
 	RunHealthCheck()
-	CheckLink(link string, wg *sync.WaitGroup)
+	CheckLink(link string)
 }
 
-func NewHealthCheck(links []string) HealthCheck {
+func NewHealthCheck(links []string, waitGroup *sync.WaitGroup) HealthCheck {
 	return &healthCheck{
-		Links: links,
+		Links:     links,
+		WaitGroup: waitGroup,
 	}
 }
 
 func (hc *healthCheck) RunHealthCheck() {
-	var wg sync.WaitGroup
 	fmt.Println("Perform website checking...")
 	for _, link := range hc.Links {
-		wg.Add(1)
-		go hc.CheckLink(link, &wg)
+		hc.WaitGroup.Add(1)
+		go hc.CheckLink(link)
 	}
-	wg.Wait()
+	hc.WaitGroup.Wait()
 	fmt.Println("Done!")
 
 }
 
-func (hc *healthCheck) CheckLink(link string, wg *sync.WaitGroup) {
-	defer wg.Done()
+func (hc *healthCheck) CheckLink(link string) {
+	defer hc.WaitGroup.Done()
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
