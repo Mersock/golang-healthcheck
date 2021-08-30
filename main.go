abc54908d2fec94fa08513d37b71484de7a6c427
Knight <knzphumthawan@gmail.com>
2021-08-30 16:32:26 +0700

    mock handler

--- main.go
+++ main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"github.com/gorilla/mux"
-	"golang-healthcheck/healthcheck"
-	"golang-healthcheck/readcsv"
+	"golang-healthcheck/handler"
 	"net/http"
+)
+
+const (
+	LineClientID    = "1656368826"
+	LineRedirectUri = "http://localhost:8080/callback"
+	LineSecret      = "1ebd163f0ff50083b449d00715603630"
 )
 
 func main() {
 	router := mux.NewRouter()
+	h := handler.NewHandler(LineClientID, LineRedirectUri, LineSecret)
 
-	r := readcsv.NewReadCSV("test.csv")
-	links := r.ReaderCSV()
-	healthcheck.NewHealthCheck(links)
+	router.HandleFunc("/", h.RedirectLogin).Methods(http.MethodGet)
+	router.HandleFunc("/callback", h.CallBack).Methods(http.MethodGet)
 
 	http.ListenAndServe(":8080", router)
 }
