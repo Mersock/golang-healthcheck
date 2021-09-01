69f5eb1f027d092696dc40704efd35da76615c1e
Knight <knzphumthawan@gmail.com>
2021-09-01 09:10:17 +0700

    fix healthcheck_test.go

--- healthcheck/healthcheck_test.go
+++ healthcheck/healthcheck_test.go
@@ -1,10 +1,16 @@
 package healthcheck
 
 import (
+	"fmt"
 	"net/http"
 	"sync"
 	"testing"
 	"time"
+)
+
+const (
+	Port   = "8080"
+	AppURl = "http://localhost:" + Port
 )
 
 var wg sync.WaitGroup
@@ -14,8 +20,9 @@ var client = &http.Client{
 }
 
 func TestRunHealthCheck(t *testing.T) {
+	baseUrl := fmt.Sprintf("%v", AppURl)
 	links := []string{"https://line.me/th/", "https://reqres.in/api/users?delay=15", "https://winning.co.th"}
-	hc := NewHealthCheck(links, &wg, client, &mutex)
+	hc := NewHealthCheck(links, &wg, client, &mutex, baseUrl)
 	sendReport := hc.RunHealthCheck()
 	//t.Logf("xx %+v", sendReport)
 	if sendReport.TotalWebsites != len(links) {
